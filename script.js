ea93af556cc072e29531808f38088b25e3c79ed2
TurtleY0da <timothy.veugelers@gmail.com>
2021-09-14 13:50:39 -0600

    First commit to the Area of a Trapezoid Javascript Project. Made by

---
+++ script.js
@@ -0,0 +1,17 @@
+// Area of a Trapezoid Calculator by Timothy Veugelers
+
+// Add Event Listener
+document.getElementById("calcBtn").addEventListener("click", calculateArea);
+
+// Functions
+function calculateArea() {
+    // Input
+    sideA = +document.getElementById("inA").value;
+    sideB = +document.getElementById("inB").value;
+    height = +document.getElementById("inHeight").value;
+    // Process
+
+    // Output
+
+
+}
